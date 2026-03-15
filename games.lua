3f8e56b61a66d14cf172bcff8e06324896370eaf
0xXrer <hoyo.a@yandex.ru>
2026-03-16 00:00:39 +0500

    Update game script paths in games.lua for "Bad Business" and "Car Crushers 2" to reflect new directory structure.

--- games.lua
+++ games.lua
@@ -2,11 +2,11 @@ return {
 	["1168263273"] = {
 		gameName = "[🎃] Bad Business | FPS ",
 		placeIds = { 3233893879 },
-		scriptUrl = "https://raw.githubusercontent.com/LKHUB-dev/Lua/refs/heads/main/scripts/bad_business.lua"
+		scriptUrl = "https://raw.githubusercontent.com/LKHUB-dev/Lua/refs/heads/main/games/bad_business.lua"
 	},
 	["1168263273"] = {
 		gameName = "[🎃] Bad Business | FPS ",
 		placeIds = { 3233893879 },
-		scriptUrl = "https://raw.githubusercontent.com/LKHUB-dev/Lua/refs/heads/main/scripts/car_crushers_2.lua"
+		scriptUrl = "https://raw.githubusercontent.com/LKHUB-dev/Lua/refs/heads/main/games/car_crushers_2.lua"
 	}
 }
