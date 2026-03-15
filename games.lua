a137129c9d2c66154450b6baa863489bb51aed22
0xXrer <hoyo.a@yandex.ru>
2026-03-16 00:01:31 +0500

    Update games.lua to add "Car Crushers 2" entry with new PlaceId and script URL, while removing the old "Bad Business" entry.

--- games.lua
+++ games.lua
@@ -4,9 +4,9 @@ return {
 		placeIds = { 3233893879 },
 		scriptUrl = "https://raw.githubusercontent.com/LKHUB-dev/Lua/refs/heads/main/games/bad_business.lua"
 	},
-	["1168263273"] = {
-		gameName = "[🎃] Bad Business | FPS ",
-		placeIds = { 3233893879 },
+	["274816972"] = {
+		gameName = "[12,000 HP🔥] Car Crushers 2 - Physics Simulation",
+		placeIds = { 654732683 },
 		scriptUrl = "https://raw.githubusercontent.com/LKHUB-dev/Lua/refs/heads/main/games/car_crushers_2.lua"
 	}
 }
