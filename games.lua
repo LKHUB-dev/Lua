8b9b8765f3e3a103024b0984bd4accec3e134c67
0xXrer <hoyo.a@yandex.ru>
2026-03-16 15:12:29 +0500

    Add "Hooked!" entry to games.lua with new PlaceId and script URL

--- games.lua
+++ games.lua
@@ -8,5 +8,10 @@ return {
 		gameName = "[12,000 HP🔥] Car Crushers 2 - Physics Simulation",
 		placeIds = { 654732683 },
 		scriptUrl = "https://raw.githubusercontent.com/LKHUB-dev/Lua/refs/heads/main/games/car_crushers_2.lua"
+	},
+	["9663968307"] = {
+		gameName = "Hooked!",
+		placeIds = { 70390793715007 },
+		scriptUrl = "https://raw.githubusercontent.com/LKHUB-dev/Lua/refs/heads/main/games/hooked.lua"
 	}
 }
